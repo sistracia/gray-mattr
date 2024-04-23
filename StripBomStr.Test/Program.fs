171ac5554e334acb109cdc42052b94ab0c2687e1
sistracia <156994197+sistracia@users.noreply.github.com>
2024-04-23 21:01:41 +0700

    Implement (strip-bom-string)[https://github.com/jonschlinkert/strip-bom-string] in F#. Thanks to ChatGPT and Claude.

---
+++ StripBomStr.Test/Program.fs
@@ -0,0 +1,24 @@
+﻿open Expecto
+open StripBomStr
+
+[<Tests>]
+let tests =
+    testList
+        "strip-bom-str"
+        [
+
+          test "should strip bom:" {
+              let actual = BOM.remove "\ufefffoo"
+              Expect.equal actual "foo" "The strings should equal"
+          }
+
+          test "should return same as input" {
+              let actual: string = BOM.remove "foo"
+              Expect.equal actual actual "The strings should equal"
+          }
+
+          ]
+
+
+[<EntryPoint>]
+let main args = runTestsWithCLIArgs [] args tests
---
+++ StripBomStr/Library.fs
@@ -0,0 +1,8 @@
+﻿namespace StripBomStr
+
+module BOM =
+    let remove (str: string) =
+        if str.Length > 0 && str.[0] = '\uFEFF' then
+            str.[1..]
+        else
+            str
